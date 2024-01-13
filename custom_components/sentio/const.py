0b6a844e4e06ee437cdcce71ca83a1d104e296a8
marathonfrank <56648569+marathonfrank@users.noreply.github.com>
2024-01-13 20:13:30 +0100

    Update const.py

--- custom_components/sentio/const.py
+++ custom_components/sentio/const.py
@@ -7,10 +7,10 @@ MANUFACTURER = "Sentiotec"
 DEVICE_NAME = "Sentio Pro Sauna Controller"
 
 SERIAL_PORT = "serial_port"
-DEFAULT_SERIAL_PORT = "/dev/ttyUSB0"
+DEFAULT_SERIAL_PORT = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB0L1AC3-if00-port0"
 BAUD_RATE = 57600
 
 MIN_SET_TEMP = 30
 MAX_SET_TEMP = 110
 
-HUMIDITY_MODELS = ("C3", "C3I", "D3", "D3I")
+HUMIDITY_MODELS = ("B3", "C3", "C3I", "D3", "D3I")
